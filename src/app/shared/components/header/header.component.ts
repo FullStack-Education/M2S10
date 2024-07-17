20aacfcd6b165366b2198c5912e98f29096d5d5f
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-07-16 21:18:58 -0300

    Atividade 1

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  usuarioLogado!: {email: string, senha: string};
 
+  constructor(private loginService: LoginService) {
+    this.usuarioLogado = this.loginService.usuarioLogado;
+  }
 }
--- src/app/shared/services/login.service.ts
+++ src/app/shared/services/login.service.ts
@@ -4,11 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
+  usuarioLogado!: {email: string, senha: string};
 
   constructor() { }
 
   login(usuario: {email: string, senha: string}) {
     sessionStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+    this.usuarioLogado = usuario;
   }
 
   logout() {
